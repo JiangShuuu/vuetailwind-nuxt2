b289c17427f7ee6e4a1a88a51c510771acd76a82
JiangShuuu <globelex65@gmail.com>
2023-06-08 22:51:15 +0800

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'daisynuxt2',
+    title: 'vue-tailwind-nuxt2',
     htmlAttrs: {
       lang: 'en',
     },
